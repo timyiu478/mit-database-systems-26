8b29dcae4e47ca67f300c2ba4ef05a163b90e3f0
Kam Wing Yiu <ctom3560@gmail.com>
2026-04-29 00:51:24 +0800

    sort executor

--- GoDB-lab-s26/execution/sort_executor.go
+++ GoDB-lab-s26/execution/sort_executor.go
@@ -3,38 +3,96 @@ package execution
 import (
 	"mit.edu/dsg/godb/planner"
 	"mit.edu/dsg/godb/storage"
+	"mit.edu/dsg/godb/common"
+	"slices"
 )
+
+type TupleWithOrdKey struct {
+	tuple storage.Tuple
+	keys []common.Value
+}
 
 // SortExecutor sorts the input tuples based on the provided ordering expressions.
 // It is a blocking operator but uses lazy evaluation (sorts on first Next).
 type SortExecutor struct {
-	// Fill me in!
+	plan *planner.SortNode
+	child Executor
+	ctx *ExecutorContext
+	err error
+	tuples []TupleWithOrdKey
+	tIdx int
 }
 
+
 func NewSortExecutor(plan *planner.SortNode, child Executor) *SortExecutor {
-	panic("unimplemented")
+	e := &SortExecutor{
+		plan: plan,
+		child: child,
+	}
+
+	return e
 }
 
 func (e *SortExecutor) PlanNode() planner.PlanNode {
-	panic("unimplemented")
+	return e.plan
 }
 
 func (e *SortExecutor) Init(ctx *ExecutorContext) error {
-	panic("unimplemented")
+	e.ctx = ctx
+	e.err = e.child.Init(ctx)
+	if e.err != nil {
+		return e.err
+	}
+	
+	e.tuples = make([]TupleWithOrdKey, 0)
+	e.tIdx = -1
+
+	desc := storage.NewRawTupleDesc(e.child.PlanNode().OutputSchema())
+
+	for e.child.Next() {
+		t := TupleWithOrdKey{
+			tuple: e.child.Current().DeepCopy(desc),
+		}
+		t.keys = make([]common.Value, len(e.plan.OrderBy))
+		for i, ord := range e.plan.OrderBy {
+			t.keys[i] = ord.Expr.Eval(e.child.Current())
+		}
+		e.tuples = append(e.tuples, t)
+	}
+	
+	sortFunc := func(a, b TupleWithOrdKey) int { 
+		for i := 0; i < len(e.plan.OrderBy); i++ {
+			ret := a.keys[i].Compare(b.keys[i]) 
+			if ret != 0 {
+				if e.plan.OrderBy[i].Direction == planner.SortOrderDescending {
+					return -1 * ret
+				}
+				return ret
+			}
+		}
+		return 0
+	}
+	slices.SortFunc(e.tuples, sortFunc)
+
+	return nil
 }
 
 func (e *SortExecutor) Next() bool {
-	panic("unimplemented")
+	if (e.tIdx+1) >= len(e.tuples) {
+		return false
+	}
+	e.tIdx++
+	return true
 }
 
 func (e *SortExecutor) Current() storage.Tuple {
-	panic("unimplemented")
+	return e.tuples[e.tIdx].tuple
 }
 
 func (e *SortExecutor) Error() error {
-	panic("unimplemented")
+	return e.err	
 }
 
 func (e *SortExecutor) Close() error {
-	panic("unimplemented")
+	return e.child.Close()
 }
