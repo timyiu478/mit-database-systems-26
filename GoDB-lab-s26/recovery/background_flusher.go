a562f640900bf09b4a3987ca9c2c83da71ebc42a
Kam Wing Yiu <ctom3560@gmail.com>
2026-05-22 14:17:08 +0800

    background flusher

--- GoDB-lab-s26/recovery/background_flusher.go
+++ GoDB-lab-s26/recovery/background_flusher.go
@@ -2,28 +2,65 @@ package recovery
 
 import (
 	"time"
+	"sync"
 
 	"mit.edu/dsg/godb/storage"
+	"mit.edu/dsg/godb/common"
 )
 
 // BackgroundFlusher is a standalone component responsible for periodically
 // flushing dirty pages from the BufferPool to disk.
 // This helps keep the Checkpoint/recovery time bounded.
 type BackgroundFlusher struct {
-	// Fill me in!
+	bp *storage.BufferPool
+	wg *sync.WaitGroup
+	ticker *time.Ticker
+	stopCh chan struct{}
 }
 
 // NewBackgroundFlusher creates a new flusher instance.
 func NewBackgroundFlusher(bp *storage.BufferPool, interval time.Duration) *BackgroundFlusher {
-	panic("unimplemented")
+	bf := &BackgroundFlusher{
+		bp: bp,
+		wg: &sync.WaitGroup{},
+		ticker: time.NewTicker(interval),
+		stopCh: make(chan struct{}),
+	}
+
+	return bf
 }
 
 // Start initiates background flushing every interval.
 func (bf *BackgroundFlusher) Start() {
-	panic("unimplemented")
+	bf.wg.Add(1)
+	go func() {
+		stop := false
+		for {
+			select {
+				case <- bf.ticker.C:
+				case <- bf.stopCh:
+					stop = true
+			}
+
+			err := bf.bp.FlushAllPages()
+			if err != nil {
+				common.DPrintf(err.Error())
+			}
+
+			if stop {
+				bf.wg.Done()
+				common.DPrintf("Stopped the background dirty page flusher")
+				return
+			}
+		}
+	}()
+	common.DPrintf("Started the background dirty page flusher")
 }
 
 // Stop signals the flusher to shut down and blocks until complete.
 func (bf *BackgroundFlusher) Stop() {
-	panic("unimplemented")
+	bf.stopCh <- struct{}{}
+	bf.wg.Wait()
+	bf.ticker.Stop()
+	close(bf.stopCh)
 }
